ff56623fd7833400e5bc59b6b662bb7bbc52abef
HoodKhan <muhammadhoodkhan9366@gmail.com>
2026-01-02 00:27:28 +0500

    next and back functionality added

--- src/components/Multiform.jsx
+++ src/components/Multiform.jsx
@@ -2,25 +2,45 @@ import React, { useState } from "react";
 import PersonalInfo from "./PersonalInfo";
 import Navbar from "./Navbar";
 import ContactInfo from "./ContactInfo";
+import LocationInfo from "./LocationInfo";
 
 function Multiform() {
-    const [signupForm,setSignupForm]=useState({
-    firstName:"",
-    lastName:"",
-    date:"",
-    month:"",
-    year:"",
-    gender:""
-  })
+  const [signupForm, setSignupForm] = useState({
+    firstName: "",
+    lastName: "",
+    date: "",
+    month: "",
+    year: "",
+    gender: "",
+  });
+
+  const [step, setStep] = useState(1);
 
   const handleChange = (e) => {
     const name = e.target.name;
-    const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+    const value =
+      e.target.type === "checkbox" ? e.target.checked : e.target.value;
     setSignupForm({
       ...signupForm,
-      [name]:value
-    })
+      [name]: value,
+    });
+  };
 
+  const nextStep = () => {
+    if (step === 3) {
+      // submit data to backend
+      return
+    } else {
+      setStep((prev) => {
+        return prev + 1;
+      });
+    }
+  };
+
+  const backStep=()=>{
+    setStep((prev)=>{
+      return prev-1;
+    })
   }
 
   return (
@@ -29,27 +49,41 @@ function Multiform() {
 
       <div className="flex justify-center items-center min-h-[calc(100vh-64px)] bg-blue-200">
         <div className="bg-[#0f264f] p-10 rounded-xl w-4/6 text-white shadow-lg">
-
-          <PersonalInfo signupForm={signupForm} handleChange={handleChange} />
-          {/* <ContactInfo /> */}
+          {
+            {
+              1: (
+                <PersonalInfo
+                  signupForm={signupForm}
+                  handleChange={handleChange}
+                />
+              ),
+              2: <ContactInfo />,
+              3: <LocationInfo />,
+            }[step]
+          }
 
           {/* Navigation Buttons */}
           <div className="flex justify-end gap-3 mt-6">
-            <button
-              type="button"
-              className="bg-gray-400 hover:bg-gray-500 text-black px-6 py-2 rounded-lg font-medium"
-            >
-              ← Back
-            </button>
+            {step > 1 && (
+              <button
+                type="button"
+                onClick={backStep}
+                className="bg-gray-400 hover:bg-gray-500 text-black px-6 py-2 rounded-lg font-medium"
+              >
+                ← Back
+              </button>
+            )}
 
             <button
               type="button"
-              className="bg-green-400 hover:bg-green-500 text-white px-6 py-2 rounded-lg font-medium"
+              onClick={nextStep}
+              className="bg-green-400 hover:bg-green-500 text-black px-6 py-2 rounded-lg font-medium"
             >
-              Next →
+              {
+                step===3? 'Submit' : 'Next →'
+              }
             </button>
           </div>
-
         </div>
       </div>
     </>
